a246345632d6816ff5d3c7f437f807eac7ab3c5b
outadoc <outadoc@gmail.com>
2014-10-10 18:32:37 +0200

    Translations update

--- assets/php/language/fr_FR.lang.php
+++ assets/php/language/fr_FR.lang.php
@@ -13,21 +13,21 @@
 	$_LANGUAGE['ACCOUNT']                  = 'Compte';
 	$_LANGUAGE['PASSWORD_CHANGE']          = 'Changer de mot de passe';
 	$_LANGUAGE['UNREGISTER']               = 'Supprimer le compte';
-	$_LANGUAGE['UNREGISTER_WARNING']       = 'Attention: une fois supprimé, tous vos skins enregistrés seront effacés. Aucune donnée ne sera conservée ni récupérable.';
+	$_LANGUAGE['UNREGISTER_WARNING']       = 'Attention: une fois votre compte supprimé, tous vos skins enregistrés seront effacés. Aucune donnée ne sera conservée ni récupérable.';
 
 	$_LANGUAGE['GRAPHMODE_SELECT']               = 'Mode graphique des skins';
-	$_LANGUAGE['GRAPHMODE_SELECT_2D']            = '2D Uniquement (plus rapide)';
-	$_LANGUAGE['GRAPHMODE_SELECT_3D']            = '3D Si disponible';
+	$_LANGUAGE['GRAPHMODE_SELECT_2D']            = '2D uniquement (plus rapide)';
+	$_LANGUAGE['GRAPHMODE_SELECT_3D']            = '3D si disponible';
 	$_LANGUAGE['MINECRAFT_LOGIN']                = 'Login Minecraft.net';
 	$_LANGUAGE['MINECRAFT_PASSWORD']             = 'Mot de passe Minecraft.net';
 	$_LANGUAGE['MINECRAFT_PASSWORD_KEY']         = 'Clé de chiffrement du mot de passe Minecraft';
-	$_LANGUAGE['MINECRAFT_LOGIN_TIP']            = 'Your minecraft.net access username or email';
-	$_LANGUAGE['TIP_NOCHANGE']                   = 'Laissez vide pour ne pas modifier';
+	$_LANGUAGE['MINECRAFT_LOGIN_TIP']            = 'Votre adresse email ou mot de passe Minecraft.net';
+	$_LANGUAGE['TIP_NOCHANGE']                   = 'Ceci sera demandé au moment de changer le skin. Laissez vide pour ne pas modifier';
 	$_LANGUAGE['MINECRAFT_PASSWORD_KEY_MISSING'] = 'Clé de chiffrement manquante';
 
 	$_LANGUAGE['SWITCHSKINMODE_SELECT']            = 'Méthode de changement de skin';
 	$_LANGUAGE['SWITCHSKINMODE_SELECT_OFFICAL']    = 'API Minecraft.net officielle';
-	$_LANGUAGE['SWITCHSKINMODE_SELECT_SKINSWITCH'] = 'SkinSwitch (plus rapide)';
+	$_LANGUAGE['SWITCHSKINMODE_SELECT_SKINSWITCH'] = 'SkinSwitch (plus pratique)';
 
 	$_LANGUAGE['PROFILES_BTN']           = 'Profils';
 	$_LANGUAGE['LOGIN']                  = 'Se connecter';
@@ -43,11 +43,11 @@
 	$_LANGUAGE['ERROR_SKIN_DIM']         = 'Dimensions invalides (64*32 ou 64*64 uniquement)';
 	$_LANGUAGE['ERROR_SKIN_TYPE']        = 'Format invalide (PNG uniquement)';
 	$_LANGUAGE['ERROR_CANVAS']           = 'Votre navigateur ne supporte pas Canvas. Impossible d\'éditer le skin.';
-	$_LANGUAGE['ERROR_MIGRATED']         = 'Vous posédez un compte mojang, utilisez votre email.';
+	$_LANGUAGE['ERROR_MIGRATED']         = 'Vous possédez un compte Mojang, utilisez votre adresse email.';
 	$_LANGUAGE['SEARCH']                 = 'Rechercher';
 	$_LANGUAGE['ACCOUNT']                = 'Compte';
-	$_LANGUAGE['ACCOUNT_MIGRATED']       = 'Votre compte a migré. Veuillez vous connecter avec votre adresse email';
-	$_LANGUAGE['EDIT']                   = 'Editer';
+	$_LANGUAGE['ACCOUNT_MIGRATED']       = 'Votre compte a été migré. Veuillez vous connecter avec votre adresse email.';
+	$_LANGUAGE['EDIT']                   = 'Éditer';
 	$_LANGUAGE['SKIN_LIST']              = 'Vos skins';
 	$_LANGUAGE['SKIN_ADD']               = 'Ajouter un skin';
 	$_LANGUAGE['SKIN_PREVIEW']           = 'Prévisualiser';
@@ -63,14 +63,14 @@
 	$_LANGUAGE['SKIN_LOAD_URL']          = 'Depuis une URL';
 	$_LANGUAGE['SKIN_LOAD_UPLOAD']       = 'Depuis votre ordinateur';
 	$_LANGUAGE['SKIN_LOAD_SUBMIT']       = 'Envoyer';
-	$_LANGUAGE['SKIN_EDIT']              = 'Editer les infos';
+	$_LANGUAGE['SKIN_EDIT']              = 'Éditer les infos';
 	$_LANGUAGE['TOOL_PEN']               = 'Crayon';
 	$_LANGUAGE['TOOL_PIPETTE']           = 'Pipette';
 	$_LANGUAGE['TOOL_ERASER']            = 'Effaceur';
 	$_LANGUAGE['TOOL_RULES']             = 'Règle';
-	$_LANGUAGE['TOOL_RESET']             = 'Reset';
-	$_LANGUAGE['TOOL_UNDO']              = 'Défaire';
-	$_LANGUAGE['TOOL_REDO']              = 'Refaire';
+	$_LANGUAGE['TOOL_RESET']             = 'RAZ';
+	$_LANGUAGE['TOOL_UNDO']              = 'Annuler';
+	$_LANGUAGE['TOOL_REDO']              = 'Rétablir';
 	$_LANGUAGE['TOOL_TEXTURE']           = 'Effet texture';
 	$_LANGUAGE['TOOL_CURSOR']            = 'Curseur';
 	$_LANGUAGE['TOOL_COPY']              = 'Copier';
